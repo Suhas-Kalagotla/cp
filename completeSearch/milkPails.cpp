d77dcf56d6185d27ff790fb88cb60b7f299481a2
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2025-11-28 14:56:09 +0530

    optimised milkpails

--- completeSearch/milkPails.cpp
+++ completeSearch/milkPails.cpp
@@ -4,22 +4,14 @@ using namespace std;
 int main() {
   int x, y, m;
   cin >> x >> y >> m;
-  int a = 0;
-  int b = 0;
-  int ansa = 0;
-  int ansb = 0;
-
   int diff = 0;
-  while (x * a <= m) {
-    int n = m - (x * a);
-    int val = n / y;
-    int value = x * a + y * val;
-    if (value > diff && value <= m) {
-      ansa = a;
-      ansb = val;
-      diff = value;
-    }
-    a++;
+  if (x > y)
+    swap(x, y);
+
+  for (int b = 0; b * y <= m; b++) {
+    int n = m - (y * b);
+    int a = n / x;
+    diff = max(diff, a * x + b * y);
   }
 
   cout << diff << endl;
